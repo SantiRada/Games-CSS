d0c2ec7ad27ed9b3ecc3fab6e7699931f2403f1f
Santiago Rada <santynrada@gmail.com>
2024-09-26 14:29:33 -0300

    Changes

--- modules/create.php
+++ modules/create.php
@@ -1,117 +1,243 @@
-<?php
-    include('config.php');
-    
-    session_start();
-    if(!isset($_SESSION['name'])): header('Location: ../index.php'); exit; endif;
-
-    $msg = $_GET['msg'] ?? "";
-    $filter = $_GET['filter'] ?? "tools";
-
-    if(isset($_POST['create-games'])):
-        $title = $_POST['title'] ?? "";
-        $description = $_POST['description'] ?? "";
-        $chips = $_POST['chips'] ?? "";
-
-        $restest = mysqli_query($con, "select * from games where title = '".$title."';");
-
-        if(mysqli_num_rows($restest) > 0):
-            $msg = "Este Juego ya está en la base de datos";
-            header('Location: create.php?filter=games&msg=' . $msg);
-            exit;
-        endif;
-
-        $res = mysqli_query($con, "insert into games (title, description, chips) values ('".$title."','".$description."','".$chips."');");
-
-        if($res): header('Location: ../index.php?msg=Creación exitosa');
-        else:
-            $msg = "Error al crear el juego, intentelo de nuevo más tarde.";
-            header('Location: create.php?filter=games&msg=' . $msg);
-            exit;
-        endif;
-    endif;
-
-    if(isset($_POST['create-tools'])):
-        $link = $_POST['link'] ?? "";
-        $img = $_POST['img'] ?? "";
-        $title = $_POST['title'] ?? "";
-        $description = $_POST['description'] ?? "";
-
-        $restest = mysqli_query($con, "select * from tools where title = '".$title."';");
-
-        if(mysqli_num_rows($restest) > 0 ):
-            $msg = "Este Herramienta ya está en la base de datos.";
-            header('Location: create.php?filter=tools&msg=' . $msg);
-            exit;
-        endif;
-
-        $res = mysqli_query($con, "insert into tools (link,img,title,description) values ('".$link."','".$img."','".$title."','".$description."');");
-
-        if($res): header('Location: ../index.php');
-        else:
-            $msg = "Error al crear la herramienta, intentelo de nuevo más tarde.";
-            header('Location: create.php?filter=tools&msg=' . $msg);
-            exit;
-        endif;
-    endif;
-?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crear <?php if($filter == "tools"): echo "Herramienta"; else: echo "Juego"; endif; ?></title>
-    <!-- STYLES -->
-    <link rel="preconnect" href="../style/style.css">
-    <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
-    <script src="https://kit.fontawesome.com/8e77509853.js" crossorigin="anonymous"></script>
-    <!-- FONTS -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
-</head>
-<body>
-    <main>
-        <h1>Crear <?php if($filter == "tools"): echo "Herramienta"; else: echo "Juego"; endif; ?></h1>
-        <form method="post" class="form-distance">
-            <?php if($filter == "games"): ?>
-                <div class="grid-2">
-                    <div class="input-content">
-                        <label for="title">Título</label>
-                        <input type="text" class="input" name="title" id="title" required>
-                    </div>
-                    <div class="input-content">
-                        <label for="description">Descripción</label>
-                        <input type="text" class="input" name="description" id="description" required>
-                    </div>
-                </div>
-                <div class="input-content">
-                        <label for="chips">Chips</label>
-                        <input type="text" class="input" name="chips" id="chips" required>
-                    </div>
-                <input type="submit" class="button-primary" name="create-games" value="Crear Juegos">
-            <?php elseif($filter == "tools"): ?>
-                <div class="grid-2">
-                    <div class="input-content">
-                        <label for="link">Link del Sitio</label>
-                        <input type="text" class="input" name="link" id="link" required>
-                    </div>
-                    <div class="input-content">
-                        <label for="img">Link a la Imagen</label>
-                        <input type="text" class="input" name="img" id="img" required>
-                    </div>
-                    <div class="input-content">
-                        <label for="title">Título</label>
-                        <input type="text" class="input" name="title" id="title" required>
-                    </div>
-                    <div class="input-content">
-                        <label for="description">Descripción</label>
-                        <input type="text" class="input" name="description" id="description" required>
-                    </div>
-                </div>
-                <input type="submit" class="button-primary" name="create-tools" value="Crear Herramienta">
-            <?php endif; ?>
-        </form>
-    </main>
-</body>
+<?php
+    include('config.php');
+    session_start();
+    
+    if(!isset($_SESSION['name'])): header('Location: ../index.php'); exit; endif;
+
+    $msg = $_GET['msg'] ?? "";
+    $filter = $_GET['filter'] ?? "tools";
+    
+    $type = "";
+    switch($filter):
+        case "tools": $type = "Herramientas"; break;
+        case "games": $type = "Juegos"; break;
+        case "slides": $type = "Slides"; break;
+        case "docs": $type = "Documetación"; break;
+    endswitch;
+
+    if(isset($_POST['create-games'])):
+
+        $title = $_POST['title'] ?? "";
+
+        $description = $_POST['description'] ?? "";
+
+        $chips = $_POST['chips'] ?? "";
+
+
+
+        $restest = mysqli_query($con, "select * from games where title = '".$title."';");
+
+
+
+        if(mysqli_num_rows($restest) > 0):
+
+            $msg = "Este Juego ya está en la base de datos";
+
+            header('Location: create.php?filter=games&msg=' . $msg);
+
+            exit;
+
+        endif;
+
+
+
+        $res = mysqli_query($con, "insert into games (title, description, chips) values ('".$title."','".$description."','".$chips."');");
+
+
+
+        if($res): header('Location: ../index.php?msg=Creación exitosa');
+
+        else:
+
+            $msg = "Error al crear el juego, intentelo de nuevo más tarde.";
+
+            header('Location: create.php?filter=games&msg=' . $msg);
+
+            exit;
+
+        endif;
+
+    endif;
+
+    if(isset($_POST['create-tools'])):
+
+        $link = $_POST['link'] ?? "";
+
+        $img = $_POST['img'] ?? "";
+
+        $title = $_POST['title'] ?? "";
+
+        $description = $_POST['description'] ?? "";
+
+
+
+        $restest = mysqli_query($con, "select * from tools where title = '".$title."';");
+
+
+
+        if(mysqli_num_rows($restest) > 0 ):
+
+            $msg = "Este Herramienta ya está en la base de datos.";
+
+            header('Location: create.php?filter=tools&msg=' . $msg);
+
+            exit;
+
+        endif;
+
+
+
+        $res = mysqli_query($con, "insert into tools (link,img,title,description) values ('".$link."','".$img."','".$title."','".$description."');");
+
+
+
+        if($res): header('Location: ../index.php');
+
+        else:
+
+            $msg = "Error al crear la herramienta, intentelo de nuevo más tarde.";
+
+            header('Location: create.php?filter=tools&msg=' . $msg);
+
+            exit;
+
+        endif;
+
+    endif;
+
+    if(isset($_POST['create-slides'])):
+        $name = $_POST['name'] ?? "";
+        $numclass = $_POST['numclass'] ?? "";
+        $link = $_POST['link'] ?? "";
+        $tags = $_POST['tags'] ?? "";
+
+        $sql = "insert into slides (name,numclass,link,tags) values ('".$name."','".$numclass."','".$link."','".$tags."');";
+        $res = mysqli_query($con, $sql);
+
+        if($res): $msg = "La presentación se ha creado con éxito.";
+        else: $msg = "Falló la conexión al servidor, intentalo de nuevo más tarde."; endif;
+
+        header('Location: create.php?filter=' . $filter . '&msg=' . $msg);
+    endif;
+
+    if(isset($_POST['create-docs'])):
+        $name = $_POST['name'] ?? "";
+        $description = $_POST['description'] ?? "";
+        $img = $_POST['img'] ?? "";
+        $link = $_POST['link'] ?? "";
+
+        $sql = "insert into docs (name,description,img,link) values ('".$name."','".$description."','".$img."','".$link."');";
+        $res = mysqli_query($con, $sql);
+
+        if($res): $msg = "La documetación se ha creado con éxito.";
+        else: $msg = "Falló la conexión al servidor, intentalo de nuevo más tarde."; endif;
+
+        header('Location: create.php?filter=' . $filter . '&msg=' . $msg);
+    endif;
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear <?php echo $type; ?></title>
+    <!-- STYLES -->
+    <link rel="preconnect" href="../style/style.css">
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+    <script src="https://kit.fontawesome.com/8e77509853.js" crossorigin="anonymous"></script>
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+</head>
+<body>
+    <main>
+        <h1>Crear <?php echo $type; ?></h1>
+        <form method="post" class="form-distance">
+            <?php if($filter == "games"): ?>
+                <div class="grid-2">
+                    <div class="input-content">
+                        <label for="title">Título</label>
+                        <input type="text" class="input" name="title" id="title" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="description">Descripción</label>
+                        <input type="text" class="input" name="description" id="description" required>
+                    </div>
+                </div>
+                <div class="input-content">
+                        <label for="chips">Chips</label>
+                        <input type="text" class="input" name="chips" id="chips" required>
+                    </div>
+                <input type="submit" class="button-primary" name="create-games" value="Crear Juegos">
+            <?php elseif($filter == "tools"): ?>
+                <div class="grid-2">
+                    <div class="input-content">
+                        <label for="link">Link del Sitio</label>
+                        <input type="text" class="input" name="link" id="link" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="img">Link a la Imagen</label>
+                        <input type="text" class="input" name="img" id="img" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="title">Título</label>
+                        <input type="text" class="input" name="title" id="title" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="description">Descripción</label>
+                        <input type="text" class="input" name="description" id="description" required>
+                    </div>
+                </div>
+                <input type="submit" class="button-primary" name="create-tools" value="Crear Herramienta">
+            <?php elseif($filter == "slides"): ?>
+                <div class="grid-2">
+                    <div class="input-content">
+                        <label for="name">Título de la Clase</label>
+                        <input type="text" class="input" name="name" id="name" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="numclass">Número de Clase</label>
+                        <select class="input" name="numclass" required>
+                            <option disabled selected>Número de Clase</option>
+                            <?php for($i = 0; $i < 60; $i++): ?>
+                                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                            <?php endfor; ?>
+                        </select>
+                    </div>
+                    <div class="input-content">
+                        <label for="link">Link de las Slides</label>
+                        <input type="text" class="input" name="link" id="link" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="tags">Tags (Separados por coma)</label>
+                        <input type="text" class="input" name="tags" id="tags" required>
+                    </div>
+                </div>
+                <input type="submit" class="button-primary" name="create-slides" value="Crear Slides">
+            <?php elseif($filter == "docs"): ?>
+                <div class="grid-2">
+                    <div class="input-content">
+                        <label for="name">Nombre de la Documetación</label>
+                        <input type="text" class="input" name="name" id="name" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="description">Descripción de la documentación</label>
+                        <input type="text" class="input" name="description" id="description" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="img">Link de la Imagen (logo)</label>
+                        <input type="text" class="input" name="img" id="img" required>
+                    </div>
+                    <div class="input-content">
+                        <label for="link">Link a la Documentación</label>
+                        <input type="text" class="input" name="link" id="link" required>
+                    </div>
+                </div>
+                <input type="submit" class="button-primary" name="create-docs" value="Crear Documentación">
+            <?php endif; ?>
+        </form>
+    </main>
+</body>
 </html>
--- slides.php
+++ slides.php
@@ -3,9 +3,7 @@
 
     include('modules/config.php');
 
-    $resgames = mysqli_query($con, "select * from games order by id desc;");
-    $restools = mysqli_query($con, "select * from tools order by id asc;");
-    $resdocs = mysqli_query($con, "select * from docs order by id asc;");
+    $resslides = mysqli_query($con, "select * from slides order by id asc;");
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -39,11 +37,11 @@
     <header id="header">
         <h2>GamesDev</h2>
         <nav>
-            <a class="btn-nav" href="#games">Juegos</a>
-            <a class="btn-nav" href="#tools">Herramientas</a>
+            <a class="btn-nav" href="index.php#games">Juegos</a>
+            <a class="btn-nav" href="index.php#tools">Herramientas</a>
             <a class="btn-nav" href="slides.php">Slides</a>
-            <a class="btn-nav" href="#docs">Docs</a>
-            <a class="btn-nav" href="#footer">Contacto</a>
+            <a class="btn-nav" href="index.php#docs">Docs</a>
+            <a class="btn-nav" href="index.php#footer">Contacto</a>
         </nav>
         <a class="btn-nav" href="<?php if(isset($_SESSION['name'])): echo "modules/close.php"; else: echo "modules/login.php"; endif; ?>"><?php if(isset($_SESSION['name'])): echo $_SESSION['name']; else: ?>Iniciar Sesión<?php endif; ?></a>
     </header>
@@ -51,8 +49,8 @@
     <main>
         <section id="games">
             <div class="title-pos">
-                <h1 class="distance">Juegos</h1>
-                <?php if(isset($_SESSION['type'])): if($_SESSION['type'] == "admin"): ?><a class="right btn-icon" href="modules/create.php?filter=games"><i class="fa-solid fa-plus"></i> Crear</a><?php endif; endif; ?>
+                <h1 class="distance">Slides</h1>
+                <?php if(isset($_SESSION['type'])): if($_SESSION['type'] == "admin"): ?><a class="right btn-icon" href="modules/create.php?filter=slides"><i class="fa-solid fa-plus"></i> Crear</a><?php endif; endif; ?>
             </div>
             <div class="sector-tabs">
                 <div class="tabs-filter">
@@ -60,57 +58,20 @@
                     <a class="chip-tabs" href="#">HTML</a>
                     <a class="chip-tabs" href="#">CSS</a>
                     <a class="chip-tabs" href="#">JS</a>
-                    <a class="chip-tabs" href="#">SQL</a>
+                    <a class="chip-tabs" href="#">React</a>
                 </div>
             </div>
             <div class="content-games">
-                <?php foreach($resgames as $row): $chips = explode(',', $row['chips']); ?>
-                    <a class="card" href="games/<?php echo $row['title']; ?>/index.php">
-                        <img src="games/<?php echo $row['title'] . "/"; ?>cover.png" alt="imagen del juego">
+                <?php foreach($resslides as $row): $tags = explode(',', $row['tags']); ?>
+                    <a class="card" href="<?php echo $row['link'] ?>" target="_blank">
+                    <iframe src="<?php echo $row['link']; ?>" frameborder="0" width="480" height="270" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                         <div class="content">
-                            <h4><?php echo $row['title']; ?></h4>
+                            <h4><?php echo $row['name']; ?></h4>
                             <div class="list-h">
-                                <?php foreach($chips as $chip): ?>
-                                    <p class="chip"><?php echo $chip; ?></p>
+                                <?php foreach($tags as $tag): ?>
+                                    <p class="chip"><?php echo $tag; ?></p>
                                 <?php endforeach; ?>
                             </div>
-                            <span><?php echo $row['description']; ?></span>
-                        </div>
-                    </a>
-                <?php endforeach; ?>
-            </div>
-        </section>
-
-        <section id="tools">
-            <div class="title-pos">
-                <h1 class="distance">Herramientas</h1>
-                <?php if(isset($_SESSION['type'])): if($_SESSION['type'] == "admin"): ?><a class="right btn-icon" href="modules/create.php?filter=tools"><i class="fa-solid fa-plus"></i> Crear</a><?php endif; endif; ?>
-            </div>
-            <div class="content-tools">
-                <?php foreach($restools as $row): ?>
-                    <a href="<?php echo $row['link']; ?>" class="card-min" target="_blank">
-                        <img src="<?php echo $row['img']; ?>" alt="imagen de la herramienta">
-                        <div class="content">
-                            <span><?php echo $row['title']; ?></span>
-                            <small><?php echo $row['description']; ?></small>
-                        </div>
-                    </a>
-                <?php endforeach; ?>
-            </div>
-        </section>
-
-        <section id="docs">
-            <div class="title-pos">
-                <h1 class="distance">Documentación</h1>
-                <?php if(isset($_SESSION['type'])): if($_SESSION['type'] == "admin"): ?><a class="right btn-icon" href="modules/create.php?filter=docs"><i class="fa-solid fa-plus"></i> Crear</a><?php endif; endif; ?>
-            </div>
-            <div class="content-tools">
-                <?php foreach($resdocs as $row): ?>
-                    <a href="<?php echo $row['link']; ?>" class="card-min" target="_blank">
-                        <img src="<?php echo $row['img']; ?>" alt="Imagen de <?php echo $row['name']; ?>">
-                        <div class="content">
-                            <span><?php echo $row['name']; ?></span>
-                            <small><?php echo $row['description']; ?></small>
                         </div>
                     </a>
                 <?php endforeach; ?>
@@ -133,7 +94,7 @@
                 var chiptabs = $(this).text();
 
                 $.ajax({
-                    url: 'modules/ajax/filtergames.php',
+                    url: 'modules/ajax/filterslides.php',
                     type: 'POST',
                     data: { type: chiptabs },
                     success: (response) => {
